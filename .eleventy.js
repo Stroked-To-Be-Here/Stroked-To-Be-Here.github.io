ec56586c9db894b42ecf778399d2b724fa5d1c86
Elli Peacock <elli.peacock@eclecticlibrary.org>
2025-01-28 11:37:47 -0500

    Updated color scheme and added basic expenses page

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
+const eleventyGoogleFonts = require("eleventy-google-fonts");
+
 module.exports = config => {
+    config.addPlugin(eleventyGoogleFonts);
+
     // Copy CSS
     config.addPassthroughCopy("src/styles.css");
     
